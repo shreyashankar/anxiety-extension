8824f3d46f7445bc570caa1bf5aedf8c5a0b1df1
Shreya <ss.shankar505@gmail.com>
2019-02-28 00:03:45 -0800

    first commit

---
+++ extension/src/components/header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { 
+      time: new Date() 
+    };
+  }
+
+  componentDidMount() { 
+    this.update = setInterval(() => {
+      this.setState({
+        time: new Date() 
+      });
+    }, 1000); 
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.update);
+  }
+
+
+  render() {
+    let { time } = this.state; // retrieve the time from state
+
+    let timeSyle = {
+      'fontFamily': 'Montserrat-Bold',
+      'fontSize': '60px',
+      'color': '#040217',
+      'letterSpacing': 0,
+      'textAlign': 'center',
+      'margin': '20px'
+    };
+
+    let textSyle = {
+      'fontFamily': 'Montserrat-SemiBold',
+      'fontSize': '32px',
+      'color': '#040217',
+      'letterSpacing': 0,
+      'textAlign': 'center',
+      'margin': '10px'
+    };
+
+    let text = "You're awesome, Shreya!"
+
+    return (
+      <div>
+          <div style={{'height': '100px'}}></div>
+          <div style={timeSyle}>{time.toLocaleTimeString()}</div>
+          <div style={textSyle}>{text}</div>
+          <div style={{'height': '50px'}}></div>
+      </div>
+    );
+  }
+}
